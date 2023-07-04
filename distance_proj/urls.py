94fc520a7315bcf735e758b792d6cae3d95d27c0
Mallika <50445449+mallika27@users.noreply.github.com>
2023-07-04 14:30:54 +0530

    Add files via upload

---
+++ distance_proj/urls.py
@@ -0,0 +1,30 @@
+"""distance_proj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from measurements.views import SearchResultsView,HomePageView,AboutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('searcher/calaculate-view', include('measurements.urls', namespace='measurements')),
+    path('searcher/', SearchResultsView.as_view(), name='search'),
+    path('index/', HomePageView.as_view(),name='index'),
+    path('about/',AboutView.as_view(),name='about')
+
+
+
+]
